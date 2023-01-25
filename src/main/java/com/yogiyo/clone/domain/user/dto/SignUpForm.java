d7cdb3636fa0b08626abc4f4bc9b25f796654b61
JxxHxxx <leesin5498@naver.com>
2023-01-25 22:26:52 +0900

    #13 회원가입 API 추가 기능
    회원가입 검증 로직 추가

--- src/main/java/com/yogiyo/clone/domain/user/dto/SignUpForm.java
+++ src/main/java/com/yogiyo/clone/domain/user/dto/SignUpForm.java
@@ -3,12 +3,25 @@ package com.yogiyo.clone.domain.user.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @NoArgsConstructor
 public class SignUpForm {
 
+    @NotBlank
+    @Pattern(regexp = "(?=.*[a-zA-Z])(?=.*[0-9]).{4,15}$",
+             message = "회원 가입 실패 / 아이디는 4 ~ 15자 사이 그리고 영문, 숫자를 포함해야 합니다.")
     private String username;
+
+    @Email(message = "회원 가입 실패 / 이메일 형식으로 입력해야 합니다.")
     private String email;
+
+    @NotBlank
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,15}$",
+             message = "회원 가입 실패 / 비밀번호는 8 ~ 15자 사이 그리고 소문자, 대문자, 숫자를 포함해야 합니다.")
     private String password;
 
     public SignUpForm(SignUpForm form, String encryptPassword) {
