58a0673c250884e946fb8539210f5b6046a476db
JxxHxxx <leesin5498@naver.com>
2023-01-25 21:55:14 +0900

    #13 회원가입 API 추가 기능
    회원가입 RequestDto 생성자 추가

--- src/main/java/com/yogiyo/clone/domain/user/dto/SignUpForm.java
+++ src/main/java/com/yogiyo/clone/domain/user/dto/SignUpForm.java
@@ -1,12 +1,19 @@
 package com.yogiyo.clone.domain.user.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class SignUpForm {
 
     private String username;
     private String email;
     private String password;
 
+    public SignUpForm(SignUpForm form, String encryptPassword) {
+        this.username = form.getUsername();
+        this.email = form.getEmail();
+        this.password = encryptPassword;
+    }
 }
