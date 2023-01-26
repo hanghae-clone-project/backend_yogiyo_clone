fa3668c6681a049d9af6dd16a3018cb0527e69f2
rylynntruth <yoy159@naver.com>
2023-01-26 19:54:26 +0900

    #44 ADMIN 로그인 연결(JWT 토큰 유지)
    타임리프 페이지를 띄우는 방식으로 로그인 연결 /admin/main으로 강제로 입장하려면 시큐리티 정책으로 인해 막힘
    새로고침은 가능하나 리다이렉트시 정책으로 막힘

--- src/main/java/com/yogiyo/clone/domain/temporary_admin/controller/AdminController.java
+++ src/main/java/com/yogiyo/clone/domain/temporary_admin/controller/AdminController.java
@@ -1,11 +1,33 @@
 package com.yogiyo.clone.domain.temporary_admin.controller;
 
+import com.yogiyo.clone.domain.user.dto.LoginForm;
+import com.yogiyo.clone.domain.user.entity.Users;
+import com.yogiyo.clone.domain.user.service.UserService;
+import com.yogiyo.clone.security.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static com.yogiyo.clone.security.jwt.JwtUtil.AUTHORIZATION_HEADER;
+
+@Slf4j
 @Controller
 @RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
 
     @RequestMapping("/login")
     public String adminLogin() {
@@ -16,4 +38,17 @@ public class AdminController {
     public String adminSignup() {
         return "admin_signup";
     }
+
+    @PostMapping("/main")
+    public String login(@RequestParam String username,@RequestParam String password,LoginForm loginForm,
+                        HttpServletRequest request, HttpServletResponse response) {
+
+        loginForm.setEmail(username);
+        loginForm.setPassword(password);
+        Users users = userService.login(loginForm);
+        response.addHeader(AUTHORIZATION_HEADER, jwtUtil.createToken(users.getUsername()));
+
+        return "admin_main";
+    }
+
 }
--- src/main/java/com/yogiyo/clone/domain/user/dto/LoginForm.java
+++ src/main/java/com/yogiyo/clone/domain/user/dto/LoginForm.java
@@ -1,7 +1,9 @@
 package com.yogiyo.clone.domain.user.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 public class LoginForm {
     private String email;
