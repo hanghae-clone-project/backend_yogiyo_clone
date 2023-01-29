63775af49326b490a46f53af010846f286c4d78d
rylynntruth <yoy159@naver.com>
2023-01-30 05:56:03 +0900

    #74 이미지 업로드 S3 연결
    업로드 img태그 추가

---
+++ src/main/java/com/yogiyo/clone/domain/temporary_admin/controller/AdminImageController.java
@@ -0,0 +1,22 @@
+package com.yogiyo.clone.domain.temporary_admin.controller;
+
+import com.yogiyo.clone.domain.temporary_admin.service.AwsS3Uploader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminImageController {
+
+    private final AwsS3Uploader awsS3Uploader;
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile multipartFile) throws IOException {
+        String fileName = awsS3Uploader.upload(multipartFile, "store");
+        return fileName;
+    }
+}
